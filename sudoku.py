9cb217cc9c96cc4699b7623473ea05f4547ac63e
AryankP1 <125646184+AryankP1@users.noreply.github.com>
2023-07-18 12:27:30 +0530

    Add files via upload

---
+++ sudoku.py
@@ -0,0 +1,80 @@
+def is_valid(board, num, row, col):
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+    
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+
+    start_row, start_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(3):
+        for j in range(3):
+            if board[i + start_row][j + start_col] == num:
+                return False
+
+    return True
+
+def find_empty_location(board):
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 0:
+                return i, j
+    return None
+
+def solve_sudoku(board):
+    empty = find_empty_location(board)
+    if not empty:
+        return True
+
+    row, col = empty
+
+    for num in range(1, 10):
+        if is_valid(board, num, row, col):
+            board[row][col] = num
+
+            if solve_sudoku(board):
+                return True
+
+            board[row][col] = 0
+
+    return False
+
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+def main():
+    board = [
+        [5, 3, 0, 0, 7, 0, 0, 0, 0],
+        [6, 0, 0, 1, 9, 5, 0, 0, 0],
+        [0, 9, 8, 0, 0, 0, 0, 6, 0],
+        [8, 0, 0, 0, 6, 0, 0, 0, 3],
+        [4, 0, 0, 8, 0, 3, 0, 0, 1],
+        [7, 0, 0, 0, 2, 0, 0, 0, 6],
+        [0, 6, 0, 0, 0, 0, 2, 8, 0],
+        [0, 0, 0, 4, 1, 9, 0, 0, 5],
+        [0, 0, 0, 0, 8, 0, 0, 7, 9]
+    ]
+
+    print("Sudoku Puzzle:")
+    print_board(board)
+
+    if solve_sudoku(board):
+        print("\nSolved Sudoku:")
+        print_board(board)
+    else:
+        print("\nNo solution exists for the given Sudoku puzzle.")
+
+if __name__ == "__main__":
+    main()
